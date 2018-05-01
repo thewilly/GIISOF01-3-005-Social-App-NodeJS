39fbed1cb8c000bf3b25495623e7d8e4aab70d09
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-05-01 11:01:46 +0200

    Added views

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -1,45 +1,49 @@
 module.exports = {
-	mongo : null,
-	app : null,
+  mongo: null,
+  app: null,
 
-	init : function(app, mongo) {
-		this.mongo = mongo;
-		this.app = app;
-	},
+  init: function(app, mongo) {
+    this.mongo = mongo;
+    this.app = app;
+  },
 
-	obtenerUsuarios : function(criterio, funcionCallback) {
-		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
-			if (err) {
-				funcionCallback(null);
-			} else {
-				var collection = db.collection('usuarios');
-				collection.find(criterio).toArray(function(err, usuarios) {
-					if (err) {
-						funcionCallback(null);
-					} else {
-						funcionCallback(usuarios);
-					}
-					db.close();
-				});
-			}
-		});
-	},
+  insertarUsuario: function(usuario, funcionCallback) {
+    this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+      if (err) {
+        console.log('[ERROR] : At inserting user in database: ' + err);
+        funcionCallback(null);
+      } else {
+        console.log('[INFO] : Inserting user in the database');
+        var collection = db.collection('usuarios');
+        collection.insert(usuario, function(err, result) {
+          if (err) {
+            console.log('[ERROR] : At inserting user in database: ' + err);
+            funcionCallback(null);
+          } else {
+            console.log('[INFO] : User inserted in database');
+            funcionCallback(result.ops[0]._id);
+          }
+          db.close();
+        });
+      }
+    });
+  },
 
-	insertarUsuario : function(usuario, funcionCallback) {
-		this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
-			if (err) {
-				funcionCallback(null);
-			} else {
-				var collection = db.collection('usuarios');
-				collection.insert(usuario, function(err, result) {
-					if (err) {
-						funcionCallback(null);
-					} else {
-						funcionCallback(result.ops[0]._id);
-					}
-					db.close();
-				});
-			}
-		});
-	},
+  obtenerUsuarios: function(criterio, funcionCallback) {
+    this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+      if (err) {
+        funcionCallback(null);
+      } else {
+        var collection = db.collection('usuarios');
+        collection.find(criterio).toArray(function(err, usuarios) {
+          if (err) {
+            funcionCallback(null);
+          } else {
+            funcionCallback(usuarios);
+          }
+          db.close();
+        });
+      }
+    });
+  }
 };
--- routes/rusuarios.js
+++ routes/rusuarios.js
@@ -1,27 +1,26 @@
 module.exports = function(app, swig, gestorBD) {
 
-  /* AW_CU_01
-   * Controller for the registration end-point.
+  /*
+   * AW_CU_01 Controller for the registration end-point.
    */
   app.get("/registrarse", function(req, res) {
     var respuesta = swig.renderFile('views/bregistro.html', {});
     res.send(respuesta);
   });
 
-  /* Controller for the register / login 
-   * 
+  /*
+   * Controller for the register / login
    */
   app.post('/usuario', function(req, res) {
-    var seguro = app.get("crypto").createHmac('sha256',
-        app.get('clave')).update(req.body.password)
-      .digest('hex');
-   
+    var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+    console.log(seguro);
     var usuario = {
       email: req.body.email,
-      name: req.doby.name,
+      name: req.body.nombre,
       password: seguro
     };
-
+    console.log("User to create -> " + usuario.name + " " + usuario.email + " " + usuario.password);
     gestorBD.insertarUsuario(usuario, function(id) {
       if (id == null) {
         res.redirect("/registrarse?mensaje=Error al registrar usuario");
