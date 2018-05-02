33f9b2e6c49c1c3980886eae393925dd0d44f2ba
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-05-02 20:31:12 +0200

    feat: servicio rest 1
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var expressSession = require('express-session');
 var crypto = require('crypto');
 var gestorBD = require("./modules/gestorBD.js");
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 
 /* BEGIN OF APP VARIABLES */
 
@@ -14,6 +15,7 @@ app.set('port', 8081);
 app.set('db', 'mongodb://admin:abcdefg@ds119258.mlab.com:19258/redsocial');
 app.set('clave', 'abcdefg');
 app.set('crypto', crypto);
+app.set('jwt',jwt);
 
 /* END OF APP VARIABLES */
 gestorBD.init(app, mongo);
@@ -24,6 +26,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  // Debemos especificar todas las headers que se aceptan. Content-Type ,
+   // token
+  next();
+ });
 
 // Loggin the error in the db.
 app.use(function(err, req, res, next) {
@@ -45,6 +56,39 @@ app.use(expressSession({
 
 /* BEGIN OF ROUTERS */
 
+// Router for API
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+  // obtener el token, puede ser un parámetro GET , POST o HEADER
+  var token = req.body.token || req.query.token || req.headers['token'];
+  if (token != null) {
+    // verificar el token
+    jwt.verify(token, 'secreto', function(err, infoToken) {
+      if (err || (Date.now() / 1000 - infoToken.tiempo) > 240) {
+        res.status(403); // Forbidden
+        res.json({
+          acceso: false,
+          error: 'Token invalido o caducado'
+        });
+        // También podríamos comprobar que intoToken.usuario existe
+        return;
+
+      } else {
+        // dejamos correr la petición
+        res.usuario = infoToken.usuario;
+        next();
+      }
+    });
+
+  } else {
+    res.status(403); // Forbidden
+    res.json({
+      acceso: false,
+      mensaje: 'No hay Token'
+    });
+  }
+});
+
 // Router for users session
 var routerUsuarioSession = express.Router();
 routerUsuarioSession.use(function(req, res, next) {
@@ -61,6 +105,7 @@ routerUsuarioSession.use(function(req, res, next) {
 /* END OF ROUTERS */
 
 require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rapiusuarios.js")(app, gestorBD);
 
 app.use(function(err, req, res, next) {
   console.log("Error producido: " + err + " req: " + req + " res: " + res);
--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -50,5 +50,23 @@ module.exports = {
       }
 
     });
+  },
+  
+  obtenerUsuarios : function(criterio, funcionCallback) {
+    this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+      if (err) {
+        funcionCallback(null);
+      } else {
+        var collection = db.collection('usuarios');
+        collection.find(criterio).toArray(function(err, usuarios) {
+          if (err) {
+            funcionCallback(null);
+          } else {
+            funcionCallback(usuarios);
+          }
+          db.close();
+        });
+      }
+    });
   }
 };
